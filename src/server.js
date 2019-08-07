a9e52264f24a57ade292c3157d5d1685c0452798
JHoo0118 <kik3078@gmail.com>
2019-08-08 01:28:09 +0900

    datamodel.prisma 설정

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import "./env";
+import { GraphQLServer } from "graphql-yoga";
+import loggar from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+server.express.use(loggar("dev"));
+
+server.start({ port: PORT }, () => console.log(`Server running on port http://localhost:${PORT}`));
